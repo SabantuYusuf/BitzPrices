f5e406d4a94a7a245857af38e35090f0a4544875
Sabantu <Sabantuyusuf@gmail.com>
2020-12-01 10:38:55 -0600

    dispaly Bitcoin price in USD, GBP, and EUR

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout'
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About BitzPrices</h1>
+            <p>App to view Bitcoin prices</p>
+        </div>
+    </Layout>
+    
+)
+
+
+export default About;
